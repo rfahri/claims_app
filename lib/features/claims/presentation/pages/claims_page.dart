0ab6c9fbe1dac06e3ef8147b45269687f4552942
rfahri <fahrirenngur@outlook.com>
2025-08-15 17:32:26 +0700

    update: add search bar on claim screen

--- lib/features/claims/presentation/pages/claims_page.dart
+++ lib/features/claims/presentation/pages/claims_page.dart
@@ -4,8 +4,16 @@ import 'package:flutter_modular/flutter_modular.dart';
 import '../cubit/claim_cubit.dart';
 import '../cubit/claim_state.dart';
 
-class ClaimPage extends StatelessWidget {
+class ClaimPage extends StatefulWidget {
   const ClaimPage({super.key});
+
+  @override
+  State<ClaimPage> createState() => _ClaimPageState();
+}
+
+class _ClaimPageState extends State<ClaimPage> {
+
+  String _searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
@@ -14,36 +22,72 @@ class ClaimPage extends StatelessWidget {
       body: BlocListener<ClaimCubit, ClaimState>(
         listener: (context, state) {
           if (state is ClaimError) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text(state.message),
-              backgroundColor: Colors.red,
-            ));
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.message),
+                backgroundColor: Colors.red,
+              ),
+            );
           }
         },
-        child: BlocBuilder<ClaimCubit, ClaimState>(
-          builder: (context, state) {
-            if (state is ClaimInitial || state is ClaimLoading) {
-              Modular.get<ClaimCubit>().fetchClaims();
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is ClaimLoaded) {
-              return ListView.builder(
-                itemCount: state.claims.length,
-                itemBuilder: (_, i) {
-                  final claim = state.claims[i];
-                  return Card(
-                    child: ListTile(
-                      title: Text(claim.title, style: TextStyle(fontWeight: FontWeight.bold),),
-                      subtitle: Text(claim.body, maxLines: 2,),
-                      onTap: () => Modular.to.pushNamed('/detail', arguments: claim),
-                    ),
-                  );
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  setState(() {
+                    _searchQuery = value;
+                  });
                 },
-              );
-            } else if (state is ClaimError) {
-              return Center(child: Text('Error: ${state.message}'));
-            }
-            return const SizedBox.shrink();
-          },
+                decoration: const InputDecoration(
+                  labelText: 'Search by title or description',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            Expanded(
+              child: BlocBuilder<ClaimCubit, ClaimState>(
+                builder: (context, state) {
+                  if (state is ClaimInitial || state is ClaimLoading) {
+                    Modular.get<ClaimCubit>().fetchClaims();
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is ClaimLoaded) {
+                    final filteredClaims = state.claims.where((claim) {
+                      return claim.title.toLowerCase().contains(_searchQuery) ||
+                          claim.body.toLowerCase().contains(_searchQuery);
+                    }).toList();
+                    if (filteredClaims.isEmpty) {
+                      return const Center(child: Text('No claims found'));
+                    }
+                    return ListView.builder(
+                      itemCount: state.claims.length,
+                      itemBuilder: (_, i) {
+                        final claim = state.claims[i];
+                        return Card(
+                          child: ListTile(
+                            title: Text(
+                              claim.title,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(claim.body, maxLines: 2),
+                            onTap: () => Modular.to.pushNamed(
+                              '/detail',
+                              arguments: claim,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else if (state is ClaimError) {
+                    return Center(child: Text('Error: ${state.message}'));
+                  }
+                  return const SizedBox.shrink();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
