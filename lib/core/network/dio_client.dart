52903f17f03eef6b47d499411e9ca7f3744b6254
rfahri <fahrirenngur@outlook.com>
2025-08-15 15:47:20 +0700

    refactor: fix dio client

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,63 +1,25 @@
-import 'package:claims_app/core/utilities/app_constant.dart';
 import 'package:dio/dio.dart';
 
-import '../errors/error_handler.dart';
-import 'network_info.dart';
-
 class DioClient {
-  late final Dio _dio;
-  final NetworkInfo _networkInfo;
+  final Dio dio;
 
-  DioClient(this._dio, this._networkInfo) {
-    _dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: _onRequest,
-        // onError: _onError,
-      ),
-    );
+  DioClient({String baseUrl = 'https://jsonplaceholder.typicode.com'})
+      : dio = Dio(BaseOptions(
+    baseUrl: baseUrl,
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 20),
+  )) {
+    dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+    ));
   }
 
-  _onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
-    if (await _networkInfo.isConnected) {
-      return handler.next(options);
-    } else {
-      return handler.reject(
-        DioException(
-          requestOptions: options,
-          response: Response(
-            requestOptions: options,
-            statusCode: ResponseCode.noInternetConnection,
-            statusMessage: ResponseMessage.noInternetConnection,
-          ),
-          error: ResponseMessage.noInternetConnection,
-        ),
-      );
-    }
-  }
-
-  Future<Response> get({
-    required String endPoint,
-    dynamic data,
-    dynamic params,
-  }) async {
-    var response = await _dio.get(
-      '${AppConstant.appUrl}$endPoint',
-      data: data,
-      queryParameters: params,
-    );
-    return response;
-  }
-
-  Future<Response> post({
-    required String endPoint,
-    dynamic data,
-    dynamic params,
-  }) async {
-    var response = await _dio.post(
-      '${AppConstant.appUrl}$endPoint',
-      data: data,
-      queryParameters: params,
-    );
-    return response;
-  }
+  Future<Response<T>> get<T>(String path) async => await dio.get(path);
+  Future<Response<T>> post<T>(String path, {dynamic data}) async =>
+      await dio.post(path, data: data);
+  Future<Response<T>> put<T>(String path, {dynamic data}) async =>
+      await dio.put(path, data: data);
+  Future<Response<T>> delete<T>(String path) async => await dio.delete(path);
 }
